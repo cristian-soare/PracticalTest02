820fb07382fb99e6d40ada4275fb555095a3935c
Cristian Soare <soare.cristian21@gmail.com>
2016-05-20 16:23:53 +0300

    server - client functionality complete

---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/CommunicationThread.java
@@ -0,0 +1,76 @@
+
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import android.util.Log;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class CommunicationThread extends Thread {
+
+    private ServerThread serverThread;
+    private Socket socket;
+
+    public CommunicationThread(ServerThread serverThread, Socket socket) {
+        this.serverThread = serverThread;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        if (socket != null) {
+            try {
+                BufferedReader bufferedReader = Utilities.getReader(socket);
+                PrintWriter printWriter = Utilities.getWriter(socket);
+                if (bufferedReader != null && printWriter != null) {
+                    Log.i(Constants.TAG, "[COMMUNICATION THREAD] Waiting for parameters from client (city / information type)!");
+                    String cmd = bufferedReader.readLine();
+
+                    if (cmd != null && !cmd.isEmpty() && cmd.equals("get_time")) {
+                        HttpClient httpClient = new DefaultHttpClient();
+                        HttpGet httpGet = new HttpGet(Constants.WEB_SERVICE_ADDRESS);
+
+                        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                        String timp = httpClient.execute(httpGet, responseHandler);
+
+                        if (serverThread.checkWaitTime(socket.getInetAddress().toString(), timp))
+                            printWriter.println(timp);
+                        else
+                            printWriter.println("time access denied\n");
+
+                        printWriter.flush();
+                    } else {
+                        Log.e(Constants.TAG, "[COMMUNICATION THREAD] Error receiving parameters from client (command type)!");
+                    }
+                } else {
+                    Log.e(Constants.TAG, "[COMMUNICATION THREAD] BufferedReader / PrintWriter are null!");
+                }
+                socket.close();
+            } catch (IOException ioException) {
+                Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + ioException.getMessage());
+                if (Constants.DEBUG) {
+                    ioException.printStackTrace();
+                }
+            }
+        } else {
+            Log.e(Constants.TAG, "[COMMUNICATION THREAD] Socket is null!");
+        }
+    }
+
+}
---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/ServerThread.java
@@ -0,0 +1,97 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import android.util.Log;
+
+import org.apache.http.client.ClientProtocolException;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+public class ServerThread extends Thread {
+
+    private int port = 0;
+    private ServerSocket serverSocket = null;
+
+    private HashMap<String, Integer> data = null;
+
+    public ServerThread(int port) {
+        this.port = port;
+        try {
+            this.serverSocket = new ServerSocket(port);
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, "An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        }
+        this.data = new HashMap<>();
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setServerSocker(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public ServerSocket getServerSocket() {
+        return serverSocket;
+    }
+
+    public synchronized boolean checkWaitTime(String ip, String timp) {
+        String minute = timp.split(":")[1];
+        Integer minint = Integer.parseInt(minute);
+
+        Integer found = data.get(ip);
+        if (found == null || !found.equals(minint)) {
+            data.put(ip, minint);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                Log.i(Constants.TAG, "[SERVER] Waiting for a connection...");
+                Socket socket = serverSocket.accept();
+                Log.i(Constants.TAG, "[SERVER] A connection request was received from " + socket.getInetAddress() + ":" + socket.getLocalPort());
+                CommunicationThread communicationThread = new CommunicationThread(this, socket);
+                communicationThread.start();
+            }
+        } catch (ClientProtocolException clientProtocolException) {
+            Log.e(Constants.TAG, "An exception has occurred: " + clientProtocolException.getMessage());
+            if (Constants.DEBUG) {
+                clientProtocolException.printStackTrace();
+            }
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, "An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+    public void stopThread() {
+        if (serverSocket != null) {
+            interrupt();
+            try {
+                serverSocket.close();
+            } catch (IOException ioException) {
+                Log.e(Constants.TAG, "An exception has occurred: " + ioException.getMessage());
+                if (Constants.DEBUG) {
+                    ioException.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
